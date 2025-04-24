4ffea163617c8123f7d7eed7bacae59e68822202
KuroSol <cyrusinout@gmail.com>
2025-04-25 00:10:33 +0900

    Change base settig for eduhab setup

--- zporta_academy_backend/zporta/settings/base.py
+++ zporta_academy_backend/zporta/settings/base.py
@@ -128,7 +128,8 @@ STATIC_URL = 'django-static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
+    "https://eduhab.com",       # Allow frontend origin with HTTPS
+    "https://www.eduhab.com",  # Allow www subdomain if used
 ]
 CORS_ALLOW_CREDENTIALS = True
 STATIC_ROOT = BASE_DIR / 'staticfiles'
