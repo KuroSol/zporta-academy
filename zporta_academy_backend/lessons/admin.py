0a2f473c1054b161de7f6aa07f89a7522e8ea052
KuroSol <cyrusinout@gmail.com>
2025-05-03 19:10:29 +0900

    fixed backend django admin editor

--- zporta_academy_backend/lessons/admin.py
+++ zporta_academy_backend/lessons/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from .models import Lesson
+from .forms import LessonAdminForm
 
 @admin.register(Lesson)
 class LessonAdmin(admin.ModelAdmin):
+    form = LessonAdminForm
     list_display = ['title', 'video_url', 'permalink', 'subject', 'created_by']
     list_filter = ['subject', 'created_by']
     search_fields = ['title', 'content']
