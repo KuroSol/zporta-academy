fbcec767313de5d3b4c75aa106e846bef527099d
KuroSol <cyrusinout@gmail.com>
2025-05-21 16:52:17 +0900

    fixed firebase api

--- zporta_academy_frontend/src/firebase.js
+++ zporta_academy_frontend/src/firebase.js
@@ -3,10 +3,10 @@ import { initializeApp } from "firebase/app";
 import { getMessaging } from "firebase/messaging";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBJLv_ajyGfVC3OvcSGmLvk_5Cg0fU21A",
+  apiKey: "AIzaSyBJLv_ajyGFVC3OvceSGmlvk_5CgQfu21A",
   authDomain: "zporta-academy.firebaseapp.com",
   projectId: "zporta-academy",
-  storageBucket: "zporta-academy.appspot.com",
+  storageBucket: "zporta-academy.firebasestorage.app",
   messagingSenderId: "521005152211",
   appId: "1:521005152211:web:74ee4bb56d3415af366126",
   measurementId: "G-FHLP3HQ0EP"
