263baf23d97896b156fc09601af446ba4259cfaf
KuroSol <cyrusinout@gmail.com>
2025-09-22 18:17:17 +0900

    Edit seo url edit utils

--- zporta_academy_backend/seo/utils.py
+++ zporta_academy_backend/seo/utils.py
@@ -1,7 +1,6 @@
-# seo/utils.py
+# C:\Users\AlexSol\Documents\zporta_academy\zporta_academy_backend\seo\utils.py
 def noindex(fn):
     def wrapped(request, *args, **kwargs):
-        response = fn(request, *args, **kwargs)
-        response['X-Robots-Tag'] = 'noindex, nofollow'
-        return response
+        # Neutralized: do not set X-Robots-Tag at all
+        return fn(request, *args, **kwargs)
     return wrapped
