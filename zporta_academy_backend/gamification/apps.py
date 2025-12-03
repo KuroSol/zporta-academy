c71e799df2614d2ecf89bf828c081a2ef70c9660
KuroSol <cyrusinout@gmail.com>
2025-12-03 09:41:52 +0900

    Fix gamification signals: connect signals in apps.py ready() method

--- zporta_academy_backend/gamification/apps.py
+++ zporta_academy_backend/gamification/apps.py
@@ -5,3 +5,7 @@ class GamificationConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "gamification"
     verbose_name = "Gamification"
+
+    def ready(self):
+        """Import signals when the app is ready"""
+        import gamification.signals  # noqa
